1efb04924426cca4fbbd10c7edcdb24e4985be30
LEE JESEOK <junelle_js@naver.com>
2022-05-13 14:40:19 +0900

    edit key name
    - email -> user_email

--- sql/[2] DDL.sql
+++ sql/[2] DDL.sql
@@ -8,8 +8,8 @@ CREATE TABLE tbl_user (
 );
 
 CREATE TABLE tbl_user_role_set (
-    email    VARCHAR2(255) NOT NULL,
+    user_email    VARCHAR2(255) NOT NULL,
     role_set VARCHAR2(255),
-    CONSTRAINT fk_role_set FOREIGN KEY ( email )
+    CONSTRAINT fk_role_set FOREIGN KEY ( user_email )
         REFERENCES tbl_user ( email )
 );
