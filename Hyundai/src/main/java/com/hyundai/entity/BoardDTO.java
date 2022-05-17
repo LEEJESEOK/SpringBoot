5f529b62e59b5a94aa5378c8406e8fbacd2a9700
olzlrlo <jleeun@naver.com>
2022-05-17 15:42:26 +0900

    지은 찐막

--- Hyundai/src/main/java/com/hyundai/entity/BoardDTO.java
+++ Hyundai/src/main/java/com/hyundai/entity/BoardDTO.java
@@ -1,7 +1,6 @@
 package com.hyundai.entity;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,13 +15,12 @@ import lombok.ToString;
 @Setter
 @ToString
 public class BoardDTO {
-	private long bno;  			// 글 번호
-	private String bname;  		// 글 작성자
-	private String uemail;		// 작성자 이메일(id)
-	private String btitle;  	// 글 제목
-	private String bcontent;	// 글 내용
-	//private Date bregdate;  	// 글 작성일
-	private int breadcount;  	// 글 조회수
+	private long bno;  					// 글 번호
+	private String bname;  				// 글 작성자
+	private String uemail;				// 작성자 이메일(id)
+	private String btitle;  			// 글 제목
+	private String bcontent;			// 글 내용
+	private LocalDateTime bregdate;  	// 글 작성일
+	private int breadcount;  			// 글 조회수
 	
-	private LocalDateTime bregdate;
 }
